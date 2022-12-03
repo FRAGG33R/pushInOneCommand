d4a8c45e26b1a83d3a3aeb3e7752fc02052806b0
FRAGG33R <aissambarchil75@gmail.com>
2022-12-03 22:34:24 +0100

    (chore) : Change Success messages color

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
  #include <time.h>
 #include <iostream>
 #include <unistd.h>
+#define BGRN "\e[1;32m"
+#define COLOR_RESET "\e[0m"
 int main(int __ac, char *__av[]){
 	clock_t __start, __end;
 	if (__ac != 2)
@@ -10,13 +12,13 @@ int main(int __ac, char *__av[]){
 		return (std::cout << "Bad commit message" << std::endl, 1);
 	if (system("git add .") != 0 )
 		return (std::cout << "< git add > command failed>" << std::endl, 1);
-	std::cout << "Successfully added" << std::endl;
+	std::cout << BGRN << "Successfully added" << COLOR_RESET << std::endl;
 	std::string __commit_command = "git commit -m \"(chore) : " + __commit_message + " \"";
 	if (system (__commit_command.c_str()) != 0 )
 		return (std::cout << "<git commit> command failed" << std::endl, 1);
-	std::cout << "Successfully commited" << std::endl;
+	std::cout << BGRN << "Successfully commited" << COLOR_RESET << std::endl;
 	if (system("git push origin master") != 0 )
 		return (std::cout << "<git push> command failed" << std::endl, 1);
-	std::cout << "Push complited successfully" << std::endl;
+	std::cout << BGRN << "Push complited successfully" << COLOR_RESET << std::endl;
 	return (0);
 }
